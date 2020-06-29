b60d6671420c4d41b40470c9f9094c70937a7e03
GarbageTierBoss <geckopath@gmail.com>
2020-06-29 01:48:05 -0400

    dash functional on positive x axis

--- Assets/Scripts/Unsorted/TheRam.cs
+++ Assets/Scripts/Unsorted/TheRam.cs
@@ -8,17 +8,21 @@ public class TheRam : MonoBehaviour //Inherit from action (IS-A)
 
     [SerializeField] KeyCode _triggerKey;
 
-    [SerializeField] float _maxRamSpeed;
-    [SerializeField] float _accelerationTime;
+    [SerializeField] float _maxRamSpeed = 4;
+    [SerializeField] float _accelerationTime = 1/6f;
     [SerializeField] float _decelerationTimeIfNoImpact;
     [SerializeField] float _recoilTimeIfHitObstacle;
+    [SerializeField] float _durationOfDash;
 
-    private bool impact;
+    private bool _isCrashed;
+    private float _timeElapsed;
+
+    Rigidbody2D _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,9 +31,35 @@ public class TheRam : MonoBehaviour //Inherit from action (IS-A)
         if (Input.GetKeyDown(_triggerKey))   //space recommended
         {
             TurnOffPlayerController();
-
+            _timeElapsed = 0;
+            StartCoroutine(AccelerateDash());
             //once process is over, turn player back on
         }
+    }
+
+    IEnumerator AccelerateDash()
+    {
+        //max speed / time
+        _rb.velocity += new Vector2(Mathf.Clamp(_maxRamSpeed / _accelerationTime, 0, _maxRamSpeed), 0);
+        _timeElapsed += Time.deltaTime;
+
+        yield return new WaitForEndOfFrame();
+
+        if (_timeElapsed <= _durationOfDash)
+        {
+            StartCoroutine(AccelerateDash());
+        }
+        else
+        {
+            StopDash();
+        }
+
+    }
+
+    private void StopDash()
+    {
+        _rb.velocity = Vector2.zero;
+        TurnOnPlayerController();
     }
 
     private void TurnOffPlayerController()
