673afcf604f18a167a715ac9a4bdd1852ffdef28
GarbageTierBoss <geckopath@gmail.com>
2020-08-10 16:39:18 -0400

    Interfaces created and organized PlayerController.cs

--- Assets/Scripts/Unsorted/Platform.cs
+++ Assets/Scripts/Unsorted/Platform.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
+public class Platform : MonoBehaviour, ICollidable
 {
-    
+    public void OnCollision()
+    {
+
+    }
 }
